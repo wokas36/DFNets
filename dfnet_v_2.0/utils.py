adb1ed14229eef191d8b19970093b1603ff7c45b
Asiri Wijesinghe <wokas36@gmail.com>
2019-12-29 15:15:16 +1100

    Update DFNets

--- dfnet_v_2.0/utils.py
+++ dfnet_v_2.0/utils.py
@@ -4,32 +4,11 @@ import scipy.sparse as sp
 import numpy as np
 from scipy.sparse.linalg.eigen.arpack import eigsh, ArpackNoConvergence
 
-import os
-import pickle as pkl
-import sys
-
-import networkx as nx
-
 def encode_onehot(labels):
     classes = set(labels)
     classes_dict = {c: np.identity(len(classes))[i, :] for i, c in enumerate(classes)}
     labels_onehot = np.array(list(map(classes_dict.get, labels)), dtype=np.int32)
     return labels_onehot
-
-
-def parse_index_file(filename):
-    """Parse index file."""
-    index = []
-    for line in open(filename):
-        index.append(int(line.strip()))
-    return index
-
-
-def sample_mask(idx, l):
-    """Create mask."""
-    mask = np.zeros(l)
-    mask[idx] = 1
-    return np.array(mask, dtype=np.bool)
 
 
 def load_data(path="../data/cora/", dataset="cora"):
@@ -39,7 +18,7 @@ def load_data(path="../data/cora/", dataset="cora"):
     idx_features_labels = np.genfromtxt("{}{}.content".format(path, dataset), dtype=np.dtype(str))
     features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
     labels = encode_onehot(idx_features_labels[:, -1])
-    samples = labels[np.random.choice(labels.shape[0], 140, replace=False)]
+    samples = labels[np.random.choice(labels.shape[0], 200, replace=False)]
 
     # build graph
     idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
@@ -68,7 +47,7 @@ def load_data_attention(path="../data/cora/", dataset="cora"):
     idx_features_labels = np.genfromtxt("{}{}.content".format(path, dataset), dtype=np.dtype(str))
     features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
     labels = encode_onehot(idx_features_labels[:, -1])
-    samples = labels[np.random.choice(labels.shape[0], 140, replace=False)]
+    samples = labels[np.random.choice(labels.shape[0], 200, replace=False)]
 
     # build graph
     idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
@@ -89,62 +68,6 @@ def load_data_attention(path="../data/cora/", dataset="cora"):
 
     return features.todense(), adj, samples, labels
 
-
-def load_data(dataset_str):
-    """Load data."""
-    FILE_PATH = os.path.abspath(__file__)
-    DIR_PATH = os.path.dirname(FILE_PATH)
-    DATA_PATH = os.path.join(DIR_PATH, 'data/')
-
-    names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
-    objects = []
-    for i in range(len(names)):
-        with open("{}ind.{}.{}".format(DATA_PATH, dataset_str, names[i]), 'rb') as f:
-            if sys.version_info > (3, 0):
-                objects.append(pkl.load(f, encoding='latin1'))
-            else:
-                objects.append(pkl.load(f))
-
-    x, y, tx, ty, allx, ally, graph = tuple(objects)
-    test_idx_reorder = parse_index_file("{}ind.{}.test.index".format(DATA_PATH, dataset_str))
-    test_idx_range = np.sort(test_idx_reorder)
-
-    if dataset_str == 'citeseer':
-        # Fix citeseer dataset (there are some isolated nodes in the graph)
-        # Find isolated nodes, add them as zero-vecs into the right position
-        test_idx_range_full = range(min(test_idx_reorder),
-                                    max(test_idx_reorder) + 1)
-        tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
-        tx_extended[test_idx_range - min(test_idx_range), :] = tx
-        tx = tx_extended
-        ty_extended = np.zeros((len(test_idx_range_full), y.shape[1]))
-        ty_extended[test_idx_range - min(test_idx_range), :] = ty
-        ty = ty_extended
-
-    features = sp.vstack((allx, tx)).tolil()
-    features[test_idx_reorder, :] = features[test_idx_range, :]
-    adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
-
-    labels = np.vstack((ally, ty))
-    labels[test_idx_reorder, :] = labels[test_idx_range, :]
-    s_val = labels[np.random.choice(labels.shape[0], 500, replace=False)]
-    
-    idx_train = range(len(y))
-    idx_val = range(len(y), len(s_val) + 500)
-    idx_test = range(len(s_val) + 500, len(s_val) + 1500)
-
-    train_mask = sample_mask(idx_train, labels.shape[0])
-    val_mask = sample_mask(idx_val, labels.shape[0])
-    test_mask = sample_mask(idx_test, labels.shape[0])
-
-    y_train = np.zeros(labels.shape)
-    y_val = np.zeros(labels.shape)
-    y_test = np.zeros(labels.shape)
-    y_train[train_mask, :] = labels[train_mask, :]
-    y_val[val_mask, :] = labels[val_mask, :]
-    y_test[test_mask, :] = labels[test_mask, :]
-
-    return adj, features, y_train, y_val, y_test, idx_train, idx_val, idx_test, train_mask, val_mask, test_mask, labels
 
 def normalize_features(mx):
     """Row-normalize sparse matrix"""
@@ -245,6 +168,12 @@ def preprocess_edge_adj_tensor(edge_adj_tensor, symmetric=True):
 
     edge_adj_out_tensor = np.array(edge_adj_out_tensor)
     return edge_adj_out_tensor
+
+
+def sample_mask(idx, l):
+    mask = np.zeros(l)
+    mask[idx] = 1
+    return np.array(mask, dtype=np.bool)
 
 
 def get_splits(y, s):
